268b9a613995436b6357fb78ffb616a282f1c8bb
ManhHungg <146363116+do2k4manhhung1807@users.noreply.github.com>
2024-12-04 09:42:31 +0700

    Update IProjectAssignment.cs

--- IRepositories/IProjectAssignment.cs
+++ IRepositories/IProjectAssignment.cs
@@ -3,7 +3,7 @@ using task_management.Models;
 
 namespace task_management.IRepositories
 {
-    public interface IProjectAssignment// : IRepository<ProjectAssignment>
+    public interface IProjectAssignment : IRepository<ProjectAssignment>
     {
         public List<Users> GetTeamMembersByProject(int id);
         public List<Project> GetProjectsByUserId(string userId);
